7cafaf6eb577f36fa0dc5e27b22eff95f02ef7cf
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-05-25 21:18:37 +0600

    update style

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,15 @@
 import { StyleSheet, View } from 'react-native';
 import { Route, Routes, Navigate } from 'react-router-native';
+//import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 // components
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
+import SignIn from './SingIn';
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
-    flexShrink: 1,
+    flex: 1,
     backgroundColor: '#e1e4e8',
   },
 });
@@ -19,6 +20,7 @@ const Main = () => {
       <AppBar />
       <Routes>
         <Route path="/" element={<RepositoryList />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </View>
