f997b892a3bf1ae623b3329b7bce837cf2309687
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-05-25 21:19:17 +0600

    add style for link

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,19 +1,31 @@
-import Constants from 'expo-constants';
 import { View, StyleSheet } from 'react-native';
+import { Link } from 'react-router-native';
 
 import AppBarTab from './AppBarTab';
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab tabName="Repositories" />
+      <Link to="/" style={styles.link}>
+        <AppBarTab tabName="Repositories" />
+      </Link>
+      <Link to="/sign-in" style={styles.link}>
+        <AppBarTab tabName="Sign in" />
+      </Link>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: Constants.statusBarHeight,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    paddingVertical: 10,
+    backgroundColor: '#24292e',
+  },
+  link: {
+    paddingHorizontal: 10,
   },
 });
 
